4988e24fae293c650a3d1f8b42eab6c4dd62a80b
Jordan Mateen <jodanmateen1991@gmail.com>
2019-05-26 12:54:23 -0400

    adding dummy data into the project card to insure store connection

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../project/ProjectList'
+import { connect } from 'react-redux'
 class Dashboard extends Component{
     render(){
+        const { projects } = this.props
+        console.log(projects)
         return(
             <div className = 'dashbaord container'>
                 <div className="row">
                     <div className="col s12 m6">
                          {/* List of Projects */}
-                        <ProjectList/>
+                        <ProjectList projects = { projects }/>
                     </div>
 
                     <div className="col s12 m5 offset-m1">
@@ -24,4 +27,19 @@ class Dashboard extends Component{
     }
 }
 
-export default Dashboard
+const mapStateToProps = ( state )=>{
+    return {
+        projects : state.project.projects
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Dashboard)
